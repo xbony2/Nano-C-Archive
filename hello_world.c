244ec27deb69aa594d8345301eb65cf5e8366b71
xbony2 <xbony2@users.noreply.github.com>
2014-12-31 19:17:28 -0500

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,6 @@
+// Author: Eric Schneider (xbony2)
+// Licensed under MIT, 2014
+// This is coded in Nano C; download the app to run it (for iOS, dunno about android).
+void main(){
+  print("hello world!");
+}
