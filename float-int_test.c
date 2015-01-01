ac76704bf4f9004d3681bd20c0b0bbd26a692493
xbony2 <xbony2@users.noreply.github.com>
2014-12-31 19:16:45 -0500

    Create float-int_test.c

---
+++ float-int_test.c
@@ -0,0 +1,13 @@
+// Author: Eric Schneider (xbony2)
+// Licensed under MIT, 2014
+// This is coded in Nano C; download the app to run it (for iOS, dunno about android).
+void main(){
+  int i = 1;
+  printf("Int i = %d", i);
+  print(); //New line
+  float f = 0.1;
+  printf("Float f = %f", f);
+  float fi = i + f;
+  print(); //New line
+  printf("Float fi = %f", fi);
+}
